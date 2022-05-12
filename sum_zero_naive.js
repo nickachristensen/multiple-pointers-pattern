1baa16487db515e68ac9725a30547d28f20bdb13
Nicholas A Christensen <nickachristensen@gmail.com>
2022-05-11 22:14:07 -0400

    refactored version

--- sum_zero_naive.js
+++ sum_zero_naive.js
@@ -10,3 +10,21 @@ function sumZero(arr){
 
 
 sumZero([-4,-3,-2,-1,0,1,2,5])
+
+// Refactored version
+
+function sumZero(arr){
+    let left = 0
+    let right = arr.length - 1
+    while(left < right){
+        let sum = arr[left] + arr[right]
+        if(sum === 0){
+            return [arr[left], arr[right]]
+        } else if(sum > 0){
+            right--
+        } else {
+            left++
+        }
+    }
+}
+
